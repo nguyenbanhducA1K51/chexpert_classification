7b70f23513eab0bd2c310f800e5f9c32bfecb660
nguyenbanhducA1K51 <nguyenduc150202@gmail.com>
2023-06-20 05:07:21 +0000

    checkpoint

--- chexpert/datasets/dataUtils.py
+++ chexpert/datasets/dataUtils.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 def balanceCE(y_score,y_true,device,beta):
    
     y_score=torch.sigmoid(y_score)
+    # print ("Beta {}".format(beta))
     # print ("y_true \n{}".format(y_true))
     # print ("y_pred \n{}".format(y_score))
     y_score=torch.clamp(y_score,min=1e-10, max=0.9999999)
@@ -27,13 +28,13 @@ def balanceCE(y_score,y_true,device,beta):
     positive_factor=torch.repeat_interleave(positive_factor, torch.tensor([y_true.size()[0]]).to(device), dim=0)
     negative_factor=torch.repeat_interleave(negative_factor, torch.tensor([y_true.size()[0]]).to(device), dim=0)
 
-    # print ("positive_factor\n{}".format(positive_factor))
-    # print ("negative_factor\n{}".format(negative_factor))
+    # print ("positive_factor (1-beta)/ (1-beta**positive+1e-10) \n{}".format(positive_factor))
+    # print ("negative_factor (1-beta)/ (1-beta**negative+1e-10)\n{}".format(negative_factor))
     loss=-positive_factor*y_true*torch.log(y_score)-(1-y_true)* negative_factor*torch.log(1-y_score)
-    return loss.to(device)
+    return loss
 
-y_score=torch.rand(5,6)
-y_true=torch.randint(0,2,(5,6))
-B=0.5
-res=balanceCE(y_score,y_true,device="cpu",beta=B)
+# y_score=torch.rand(5,6)
+# y_true=torch.randint(0,2,(5,6))
+# B=0.5
+# res=balanceCE(y_score,y_true,device="cpu",beta=B)
 
